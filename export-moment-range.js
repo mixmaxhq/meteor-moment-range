60db017c61fbe747fb0e8e696bf136ceaccd8c7c
Kevin Bennett <kevbuk@gmail.com>
2014-04-03 23:35:14 +0800

    removed extraneous bits

--- export-moment-range.js
+++
@@ -1,6 +0,0 @@
-//Not sure if needed...
-/*
-if (typeof Package !== "undefined") {
-  moment = this.moment;
-}
-*/
--- package.js
+++ package.js
@@ -6,5 +6,4 @@ Package.on_use(function (api, where) {
   where = where || ['client', 'server'];
   api.use('moment', where);
   api.add_files('lib/moment-range/lib/moment-range.js', where);
-  api.add_files('export-moment-range.js', where);
 });
