1e776b03b4c795b96f958b8d0fe5fcff20f43938
Jeff Wear <jeff@mixmax.com>
2015-01-23 11:58:13 -0800

    Update moment-range to 1.0.2.
    And adopt the post-0.9 Meteor package format:
    http://joshowens.me/how-to-upgrade-your-meteor-js-package-to-work-for-0-9/.

--- package.js
+++ package.js
@@ -1,9 +1,12 @@
 Package.describe({
-  summary: "Fancy date ranges for Moment.js packaged for Meteor."
+  summary: "Fancy date ranges for Moment.js packaged for Meteor.",
+  version: '1.0.2',
+  name: 'kevbuk:moment-range',
+  git: 'https://github.com/kevb/meteor-moment-range'
 });
 
-Package.on_use(function (api, where) {
+Package.onUse(function (api, where) {
   where = where || ['client', 'server'];
-  api.use('moment', where);
-  api.add_files('lib/moment-range/lib/moment-range.js', where);
+  api.use('mrt:moment', where);
+  api.addFiles('lib/moment-range/lib/moment-range.js', where);
 });
